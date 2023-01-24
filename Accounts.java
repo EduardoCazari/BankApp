84b9ff87866877f0b97f3ec0a8ac9cb09fb0b73c
EduardoCazari <eduardocazari@proton.me>
2023-01-24 16:28:52 -0500

    Add new methods at Accounts class and created savings instance

--- Accounts.java
+++ Accounts.java
@@ -2,6 +2,7 @@ class Accounts {
 
     private double deposit;
     private double balance;
+    private double interestRate;
 
     public void setBalance(double balance) {
      this.balance = balance;
@@ -13,6 +14,14 @@ class Accounts {
 
     public double moneyIn() {
      return balance + deposit;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
     }
 
     
--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ class Main {
 
     public static void main(String[] args) {
 
+        // Login session
         Login login = new Login();
         Scanner sc = new Scanner(System.in);
 
@@ -14,16 +15,29 @@ class Main {
         login.setPassword(sc.nextLine());
 
         login.confirmation();
+        //-------------------------------------------------
 
+        // Checking Account
         Accounts checking = new Accounts();
         checking.setBalance(0.0);
 
-        System.out.println("Type the ammount of your first deposit:");
+        System.out.println("Type the ammount of your deposit to your checking account:");
         checking.setDeposit(sc.nextDouble());
 
+        System.out.println("Your checking account balance is " + checking.moneyIn());
+        //-------------------------------------------------------------------
 
-        System.out.println("Your balance is " + checking.moneyIn());
+        // Savings Account
+        Accounts savings = new Accounts();
+        savings.setBalance(0.0);
+        savings.setInterestRate(12.0);
+        
+        System.out.println("Type the ammount of your deposit to your savings account:");
+        savings.setDeposit(sc.nextDouble());
 
+        System.out.println("Your savings account balance is " + checking.moneyIn() + " at an interest rate of " + savings.getInterestRate() + "%");
+
+        sc.close();
 
     }
 }
