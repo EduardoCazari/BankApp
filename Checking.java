4224c30bd6b35397f5f9ea8d43cf348df7abb5d4
EduardoCazari <eduardocazari@proton.me>
2023-01-31 14:34:40 -0500

    Changes

---
+++ Checking.java
@@ -0,0 +1,28 @@
+public class Checking extends Accounts {
+
+    public Checking(double deposit, double withdraw, double balance) {
+        super(deposit, withdraw, balance);
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setDeposit(double deposit) {
+        this.deposit = deposit;
+    }
+
+    public void setWithdraw(double withdraw) {
+        this.withdraw = withdraw;
+    }
+
+
+    public double moneyIn() {
+        return balance + deposit;
+    }
+
+    public double moneyOut() {
+        return balance - withdraw;
+    }
+}
+
---
+++ Savings.java
@@ -0,0 +1,39 @@
+public class Savings extends Accounts {
+
+    double interestRate;
+
+    public Savings(double deposit, double withdraw, double balance, double ineterstRate) {
+        super(deposit, withdraw, balance);
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setDeposit(double deposit) {
+        this.deposit = deposit;
+    }
+
+    public void setWithdraw(double withdraw) {
+        this.withdraw = withdraw;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+
+    public double moneyIn() {
+        return balance + deposit;
+    }
+
+    public double moneyOut() {
+        return balance - withdraw;
+    }
+   
+
+}
